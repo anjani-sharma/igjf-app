a8bc38900d24958b5abe1097e3f4bb316f45f323
Anjani Sharma <anjani.sharma1@gmail.com>
2025-09-23 09:04:36 +0100

    Add root server.js to redirect to backend server

---
+++ server.js
@@ -0,0 +1,7 @@
+/**
+ * Root server.js file that redirects to the backend server
+ * This file is used by Render to start the application
+ */
+
+// Simply require the backend server.js file
+require('./backend/server.js');
